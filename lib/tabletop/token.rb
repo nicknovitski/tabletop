b002a725d7fc56cb18fc4fc4c75496b7d91dfa68
Nick Novitski <nicknovitski@gmail.com>
2011-08-14 16:27:58 -0700

    add TokenStack#remove

--- lib/tabletop/token.rb
+++ lib/tabletop/token.rb
@@ -1,9 +1,13 @@
 module Tabletop
+  
+  class NotEnoughTokensError < ArgumentError
+  end
+  
   class TokenStack
     attr_accessor :count
     
-    def initialize
-      @count = 1
+    def initialize(n = 1)
+      @count = n
     end
     
     def add(n = 1)
@@ -11,5 +15,22 @@ module Tabletop
       @count += n
     end
     
+    def remove(n=1)
+      raise ArgumentError unless Fixnum === n and n > 0
+      if n > @count
+        n_t, c_t = "token", "token"
+        
+        n_t << "s" if n > 1 or n == 0
+        
+        c_t << "s" if @count > 1 or @count == 0
+        
+        c = @count > 0 ? @count : "no" 
+        errmsg = "tried to remove #{n} #{n_t} from a stack with #{c} #{c_t}"
+        raise NotEnoughTokensError, errmsg
+      end
+      @count -= n
+    end
   end
+  
+
 end
--- spec/token_spec.rb
+++ spec/token_spec.rb
@@ -5,6 +5,11 @@ module Tabletop
 
     describe "#count" do
       it {subject.count.should == 1}
+      it "can be set on instantiation" do
+        (1..5).each do |v|
+          TokenStack.new(v).count.should == v
+        end
+      end
     end
 
     describe "#add" do
@@ -47,7 +52,12 @@ module Tabletop
       end
 
       context "when called with one argument" do
-        it "descreases the count by that argument" 
+        it "decreases the count by that argument" do
+          (1..5).each do |i|
+            s = TokenStack.new(5)
+            expect {s.remove(i)}.to change{s.count}.from(5).to(5-i)
+          end
+        end
         
         it "does not accept non-integers" do
           expect { subject.remove(0.1) }.to raise_error(ArgumentError)
@@ -59,7 +69,17 @@ module Tabletop
           subject.remove(1)
         end
         
-        it "raises an error when trying to "
+        it "raises an error when trying to remove too many" do
+          expect { subject.remove(2) }.to raise_error(
+            NotEnoughTokensError,
+            /tried to remove 2 tokens from a stack with 1 token/)
+          expect { TokenStack.new(2).remove(3) }.to raise_error(
+            NotEnoughTokensError,
+            /tried to remove 3 tokens from a stack with 2 tokens/)
+          expect { TokenStack.new(0).remove(1) }.to raise_error(
+            NotEnoughTokensError,
+            /tried to remove 1 token from a stack with no tokens/)
+        end
       end
     end
     describe "#move" do
