39ef588cf6320b414f317f98c57b7563c208ed2e
Nick Novitski <nicknovitski@gmail.com>
2013-06-23 15:40:37 -0700

    randomize spec runs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,7 @@ require 'pry'
 
 RSpec.configure do |config|
   config.color_enabled = true
+  config.order = 'random'
   config.tty = true
   config.expect_with :rspec do |c|
     c.syntax = :expect
