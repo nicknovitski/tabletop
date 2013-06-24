fae6db067c6d12a61f0f39ba1f579fcbfff7bb6c
Nick Novitski <nicknovitski@gmail.com>
2013-06-23 20:27:33 -0700

    simple Tables can be built with arrays

--- lib/tabletop/table.rb
+++ lib/tabletop/table.rb
@@ -5,6 +5,8 @@ module Tabletop
     end
     
     def [](index)
+      index = index-1 if @collection.is_a? Array
+      raise KeyError if index < 0
       @collection.fetch(index) do
         enum_and_value = @collection.detect { |k,v| Array(k).include?(index) }
         raise KeyError unless enum_and_value
--- spec/tabletop/table_spec.rb
+++ spec/tabletop/table_spec.rb
@@ -1,10 +1,13 @@
 require 'spec_helper'
 
+require 'tabletop/table'
+
 module Tabletop
   describe Table do
 
-    subject(:atmo_table) {
-      Table.new(
+    context 'when initialized with a hash' do
+      let(:atmo_hash) {
+        {
         2 => 'Corrosive',
         3 => 'Inert gas',
         4 => 'Airless or thin atmosphere',
@@ -12,22 +15,40 @@ module Tabletop
         10 => 'Thick atmosphere, breathable with a pressure mask',
         11 => 'Invasive, toxic atmosphere',
         12 => 'Corrosive and invasive atmosphere'
-      )
-    }
+      }
+      }
 
-    describe '#[]' do
-      it 'accesses values in the enumerable passed to the constructor' do
-        expect(atmo_table[2]).to eq 'Corrosive'
-      end
+      subject(:atmo_table) { Table.new(atmo_hash) }
 
-      it 'accesses values in ranges' do
-        (5..9).each do |i|
-          expect(atmo_table[i]).to eq 'Breathable mix'
+      describe '#[]' do
+        it 'delegates to the hash' do
+          expect(atmo_table[2]).to eq atmo_hash[2]
+        end
+
+        it 'handles ranges as keys' do
+          (5..9).each do |i|
+            expect(atmo_table[i]).to eq atmo_hash[5..9]
+          end
+        end
+
+        it 'raises KeyError for anything not in the table' do
+          expect { atmo_table[13] }.to raise_exception KeyError
         end
       end
-      
-      it 'raises KeyError for anything not in the table' do
-        expect { atmo_table[13] }.to raise_exception KeyError
+    end
+
+    context 'when initialized with an array' do
+      subject(:simple_table) { Table.new([:foo, :bar]) }
+      describe '#[]' do
+        it 'indexes from 1, not zero' do
+          expect(simple_table[1]).to be :foo
+          expect(simple_table[2]).to be :bar
+        end
+
+        it 'raises KeyError for anything not in the table' do
+          expect { simple_table[0] }.to raise_exception KeyError
+          expect { simple_table[3] }.to raise_exception KeyError
+        end
       end
     end
   end
