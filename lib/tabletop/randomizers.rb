8ed0da289f83de750b08da42bbed257fa8fd018b
Nick Novitski <nicknovitski@gmail.com>
2014-09-20 02:28:49 +0000

    remove NumericRandomizer
    I can't remember where I was going with it.

--- lib/tabletop/randomizers.rb
+++ lib/tabletop/randomizers.rb
@@ -1,5 +1,4 @@
 require_relative 'randomizers/randomizer'
-require_relative 'randomizers/numeric_randomizer'
 require_relative 'randomizers/coin'
 require_relative 'randomizers/die'
 require_relative 'randomizers/fudge_die'
--- lib/tabletop/randomizers/numeric_randomizer.rb
+++
@@ -1,6 +0,0 @@
-require_relative 'randomizer'
-
-module Tabletop
-  class NumericRandomizer < Randomizer
-  end
-end
